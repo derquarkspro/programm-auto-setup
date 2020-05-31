ecb9a416ea84ca97267d97304ca37c8580271933
Der_Quarks_Pro <65791753+derquarkspro@users.noreply.github.com>
2020-05-31 14:52:17 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import os
 from selenium import webdriver
+PATH = "C:\Program Files (x86)\Google\Chrome\chromedriver.exe"
 
 if __name__ == "__main__" :
 	print('Hello World!')
 	os.system('notepad')
-	browser = webdriver.Chrome()
+	browser = webdriver.Chrome(PATH)
 	browser.get('https://www.oracle.com/java/technologies/javase-jdk14-downloads.html')
